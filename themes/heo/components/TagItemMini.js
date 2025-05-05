d526f8e611c3fab74520fb94816ca81e658bbb2d
coldyael <yaoliu0303@gmail.com>
2025-05-05 22:47:53 +0800

    Update TagItemMini.js

--- themes/heo/components/TagItemMini.js
+++ themes/heo/components/TagItemMini.js
@@ -7,15 +7,16 @@ const TagItemMini = ({ tag, selected = false }) => {
       key={tag}
       href={selected ? '/' : `/tag/${encodeURIComponent(tag.name)}`}
       passHref
-      className={
-        'cursor-pointer inline-block hover:text-white hover:bg-indigo-600 dark:hover:bg-yellow-600 px-2 py-1 rounded-2xl dark:text-white duration-200 text-sm whitespace-nowrap '
-      }>
-      <div className='font-light flex items-center'>
-        <HashTag className='stroke-2 mr-0.5 w-3 h-3' />{' '}
-        {tag.name + (tag.count ? `(${tag.count})` : '')}{' '}
-      </div>
+      className={`cursor-pointer inline-block rounded hover:bg-gray-500 hover:text-white duration-200
+        mr-2 py-0.5 px-1 text-xs whitespace-nowrap dark:hover:text-white
+         ${selected
+        ? 'text-white dark:text-gray-300 bg-black dark:bg-black dark:hover:bg-gray-900'
+        : `text-gray-600 hover:shadow-xl dark:border-gray-400 notion-${tag.color}_background dark:bg-gray-800`}` }>
+
+      <div className='font-light dark:text-gray-400'>{selected && <i className='mr-1 fas fa-tag'/>} {tag.name + (tag.count ? `(${tag.count})` : '')} </div>
+
     </Link>
-  )
+  );
 }
 
 export default TagItemMini
