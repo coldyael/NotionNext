020de8abce195f626ee568a00ef8f3922b5ce283
coldyael <yaoliu0303@gmail.com>
2025-05-06 23:52:09 +0800

    Update BlogPostCard.js

--- themes/heo/components/BlogPostCard.js
+++ themes/heo/components/BlogPostCard.js
@@ -38,7 +38,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
           (POST_TWO_COLS ? '2xl:h-80 2xl:flex-col' : '') + // 高度从 h-96 → h-80
           ' wow fadeInUp border bg-white dark:bg-[#1e1e1e] flex mb-4 flex-col h-[18rem] md:h-44 md:flex-row group w-full dark:border-gray-600 hover:border-indigo-600 dark:hover:border-yellow-600 duration-300 transition-colors justify-between overflow-hidden rounded-xl'
         }>
-        
+
         {/* 图片封面 */}
         {showPageCover && (
           <Link href={post?.href} passHref legacyBehavior>
@@ -55,18 +55,18 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
             </a>
           </Link>
         )}
-      
+
         {/* 文字区块 */}
         <div className={
             (POST_TWO_COLS ? '2xl:p-4 2xl:h-48 2xl:w-full' : '') +
             ' flex p-4 flex-col justify-between h-48 md:h-full w-full md:w-7/12 space-y-2' // 减少内边距 p-6 → p-4，添加紧凑间距
           }>
-          
+
           {/* 标题区块 */}
           <div className="space-y-1"> {/* 减少垂直间距 */}
-            <Link 
-              href={post?.href} 
-              passHref 
+            <Link
+              href={post?.href}
+              passHref
               className={
                 ' group-hover:text-indigo-700 dark:hover:text-yellow-700 dark:group-hover:text-yellow-600 text-black dark:text-gray-100  line-clamp-2 replace cursor-pointer text-xl font-extrabold leading-tight'
               }>
@@ -78,33 +78,34 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
               )}
               <span className='menu-link'>{post.title}</span>
             </Link>
-            
-            {/* 日期 */}
-            <h2 className='text-sm text-gray-500'>
-              <Link
-                href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}
-                passHref
-                className='font-light hover:underline cursor-pointer text-sm leading-4 mr-3'>
-                <i className='far fa-clock mr-1' />
-                {post.date?.start_date || post.lastEditedDay}
-              </Link>
-            </h2>
-          </div>
-      
-          {/* 分类标签区块 */}
-          <div className='flex flex-col gap-2'> {/* 改用列布局 */}
-            {post.category && (
-              <Link href={`/category/${post.category}`} className='text-sm hover:underline'>
-                <i className='mr-1 far fa-folder' />
-                {post.category}
-              </Link>
-            )}
-            
-            <div className='flex flex-wrap gap-2'>
-              {post.tagItems?.map(tag => <TagItemMini key={tag.name} tag={tag} />)}
+
+            {/* 新增的日期和分类区块 */}
+            <div className='flex items-center justify-between gap-4 mt-2'>
+              {/* 日期 */}
+              {post.date?.start_date && (
+                <div className='flex items-center text-sm text-gray-500 dark:text-gray-400'>
+                  <i className='far fa-clock mr-1' />
+                  <span>{post.date?.start_date || post.lastEditedDay}</span>
+                </div>
+              )}
+
+              {/* 分类 */}
+              {post.category && (
+                <Link
+                  href={`/category/${post.category}`}
+                  className='flex items-center text-sm text-gray-600 dark:text-gray-300 hover:text-indigo-700 dark:hover:text-indigo-400 transition-colors'
+                >
+                  <i className='far fa-folder mr-1' />
+                  {post.category}
+                </Link>
+              )}
             </div>
           </div>
-        </div>
+          {/* 文章标签 */}
+          <div className='flex flex-wrap gap-2 mt-2'>
+            {post.tagItems?.map(tag => <TagItemMini key={tag.name} tag={tag} />)}
+          </div>
+          </div>
       </div>
     </article>
   )
