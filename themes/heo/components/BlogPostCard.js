6b56a69ed99be67c9eeb45c3557782ee1b9ee8eb
coldyael <yaoliu0303@gmail.com>
2025-05-06 23:05:01 +0800

    Update BlogPostCard.js

--- themes/heo/components/BlogPostCard.js
+++ themes/heo/components/BlogPostCard.js
@@ -61,7 +61,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
             (POST_TWO_COLS ? '2xl:p-4 2xl:h-48 2xl:w-full' : '') +
             ' flex p-4  flex-col justify-between h-48 md:h-full w-full md:w-6/12'
           }>
-          <header className="space-y-1"> {/* 添加紧凑间距 */}
+
           {/* 标题和图标 */}
           <Link
             href={post?.href}
@@ -106,7 +106,6 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
             </div>
           </div>
         </div>
-        </header>
       </div>
     </article>
   )
